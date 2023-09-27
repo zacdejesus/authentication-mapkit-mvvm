4c33182fc56b357bfde27cbc7bb5bc473db70796
Alejandro De Jesus <zadkielsi@outlook.com>
2023-09-27 16:36:17 -0300

    Initial Commit

---
+++ authentication+mapkit+mvvm/authentication_mapkit_mvvmApp.swift
@@ -0,0 +1,17 @@
+//
+//  authentication_mapkit_mvvmApp.swift
+//  authentication+mapkit+mvvm
+//
+//  Created by Z on 27/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct authentication_mapkit_mvvmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
