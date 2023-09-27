63976f9636bb266dc6cd621ab49de8e33b708bb3
Alejandro De Jesus <zadkielsi@outlook.com>
2023-09-27 16:54:20 -0300

    files added

--- authentication+mapkit+mvvm/ContentView-ViewModel.swift
+++ authentication+mapkit+mvvm/ContentView-ViewModel.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import LocalAuthentication
+import MapKit
+
+extension ContentView {
+    @MainActor class ViewModel: ObservableObject {
+        @Published var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 50, longitude: 0), span: MKCoordinateSpan(latitudeDelta: 25, longitudeDelta: 25))
+        @Published private(set) var locations: [Location]
+        @Published var selectedPlace: Location?
+        @Published var isUnlocked = false
+        
+        let savePath = FileManager.documentsDirectory.appendingPathComponent("SavedPlaces")
+        
+        init() {
+            do {
+                let data = try Data(contentsOf: savePath)
+                locations = try JSONDecoder().decode([Location].self, from: data)
+            } catch {
+                locations = []
+            }
+        }
+    }
+}
--- authentication+mapkit+mvvm/ContentView.swift
+++ authentication+mapkit+mvvm/ContentView.swift
@@ -5,17 +5,15 @@
 //  Created by Z on 27/09/2023.
 //
 
+import MapKit
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var viewModel = ViewModel()
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        
     }
 }
 
